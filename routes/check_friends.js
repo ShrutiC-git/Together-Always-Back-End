4e6c4e0377198ce8873d8d5c4d379be4091d35fd
Shruti Chaturvedi <shrutichaturvedi16.sc@gmail.com>
2020-12-06 03:08:47 +0530

    Delete this file later

--- routes/check_friends.js
+++ routes/check_friends.js
@@ -9,7 +9,7 @@ var token = 'AAAAAAAAAAAAAAAAAAAAADJ8KQEAAAAAi7OztS1ohQL9RRT0MNkPXhmwsEQ%3Dfo0tv
 friends_router.route('/')
     .get(async (req, res, next) => {
         console.log('The screenaname is ', req.user.screenName)
-        const response = await needle('get', `https://api.twitter.com/1.1/followers/list.json?screen_name=${req.user.screenName}&count=2`,
+        const response = await needle('get', `https://api.twitter.com/1.1/followers/list.json?screen_name=${req.user.screenName}&count=100`,
             {
                 headers: {
                     "authorization": `Bearer ${token}`
@@ -20,35 +20,41 @@ friends_router.route('/')
             const friends = response.body.users;
             //console.log(friends);
             friends.forEach(function (friend) {
-                console.log(friend.name)
+                //console.log(friend.name)
                 MongoClient.connect(url, async function (err, db) {
                     var database = db.db("Twitter-MH");
                     var collection = database.collection('followers');
                     const filter = { screenName: req.user.screenName }
-                    const tweets = await needle('get', `https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=${friend.name}&count=1`, {
+                    const tweets = await needle('get', `https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=${friend.screen_name}&count=10`, {
                         headers: {
                             "authorization": `Bearer ${token}`
                         }
                     })
-                    
-                    if (tweets.body) {
-                        console.log(tweets.body[0].text)
 
+                    if (tweets.body) {
+                        //console.log('Body is going to be', tweets.body[0].text)
+                        const store = tweets.body;
+                        store.forEach(async function (value) {
+                            //console.log('Here we are receiving', value.text)
+                            const insertDoc = {
+                                user_screenname: req.user.screenName,
+                                follower_screenname: friend.name,
+                                follower_location: friend.location,
+                                description: friend.description,
+                                tweet: value.text
+                            }
+                            const result = await collection.insertOne(insertDoc);
+                            //console.log(`${result.insertedCount} documents`)
+                        })
                     }
                     else {
                         throw new Error('Unsuccessful request')
                     }
-
-
-                    /*                     const insertDoc = {
-                                            user_screenname: req.user.screenName,
-                                            follower_screenname: friend.name,
-                                            follower_location: friend.location,
-                                            description: friend.description,
-                                        }
-                                        const result = await collection.insertOne(insertDoc);
-                                        console.log(`${result.insertedCount} documents`) */
                 })
+            })
+            res.json({
+                success: true,
+                message: 'users are well'
             })
         }
     })
